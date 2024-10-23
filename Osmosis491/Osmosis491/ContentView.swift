e994579c94f83389331a839986fb19f72a26f286
Juli Nazzario <98191405+jnazzario@users.noreply.github.com>
2024-10-23 14:51:48 -0700

    Update ContentView.swift

--- Osmosis491/Osmosis491/ContentView.swift
+++ Osmosis491/Osmosis491/ContentView.swift
@@ -1,5 +1,3 @@
-import SwiftUI
-
 struct ContentView: View {
     @State private var naviPath = NavigationPath()
     var body: some View {
@@ -13,16 +11,16 @@ struct ContentView: View {
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
                     .opacity(1.0)
-                
+
                 VStack {
                     Spacer()
-                
+
                     // App title
                     Text("Osmosis")
                         .font(.custom("TAN - MON CHERI", size: 50))
                         .foregroundColor(.black)
                         .padding(.top, 39)
-                    
+
                     // Card Suit Symbols below the title
                     HStack(spacing: 0.25) {
                         Image(systemName: "suit.club.fill")
@@ -35,8 +33,8 @@ struct ContentView: View {
                     .font(.system(size: 45))
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding(.leading, 9)
-                    
-                    
+
+                    // Link to GameView
                     NavigationLink(destination: GameView()) {
                         Text("new deal")
                             .font(.custom("TAN - MON CHERI", size: 30))
@@ -44,9 +42,11 @@ struct ContentView: View {
                             .frame(maxWidth: .infinity, alignment: .leading)
                             .padding(.leading, 125)
                     } // NavLink End line 33
+                    
                     .navigationBarHidden(true)// Hide navigation bar on the main view
                     .navigationBarBackButtonHidden(true)
-                    
+
+                    // Link to InstructionsView
                     NavigationLink(destination: InstructionsView()){
                         Text("how to")
                             .font(.custom("TAN - MON CHERI", size: 30))
@@ -60,30 +60,11 @@ struct ContentView: View {
             } // ZStack end lin 6
            .navigationBarHidden(true)// Hide navigation bar on the main view
            .navigationBarBackButtonHidden(true)
-        } // NavigationView
+        } // NavigationStack end
     }
 }
-
-// Extension to use hex colors in SwiftUI
-extension Color {
-    init?(hex: String) {
-        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-
-        var rgb: UInt64 = 0
-        Scanner(string: hexSanitized).scanHexInt64(&rgb)
-
-        let red = Double((rgb >> 16) & 0xFF) / 255.0
-        let green = Double((rgb >> 8) & 0xFF) / 255.0
-        let blue = Double(rgb & 0xFF) / 255.0
-
-        self.init(red: red, green: green, blue: blue)
-    }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
